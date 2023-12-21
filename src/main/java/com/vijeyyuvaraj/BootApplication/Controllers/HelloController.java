335771d90eef2070ed19f762a522f69df7e41f24
Vj <vijeyyuvaraj@gmail.com>
2023-12-21 14:06:38 +0530

    Project initialized

---
+++ src/main/java/com/vijeyyuvaraj/BootApplication/Controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.vijeyyuvaraj.BootApplication.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String welcomeMethod(){
+        return " Welcome to Boot Application!!! restarted ";
+    }
+
+}
